bb95ab6c05f27d94e6b205db8af93932ed00d547
Hayci Natalia Zuniga Ramirez <97977137+SMIS947720@users.noreply.github.com>
2022-02-13 17:08:47 -0600

    Update App.vue

--- practica4componentes/src/App.vue
+++ practica4componentes/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Longitud/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Longitud from ' .components/Longitud.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+     Longitud
   }
 }
 </script>
