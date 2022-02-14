07f2d45dc2d64542b9e80988f2034d8d2b4313ac
Christopher Alberto Murillo Castro <97977019+SMIS903520@users.noreply.github.com>
2022-02-13 21:50:32 -0600

    Create Navegador.vue
    Agregar un navegador

---
+++ practica4componentes/src/components/Navegador.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Convertidores</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="longitud" >longitud</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="convertMoneda">Moneda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="almacenamiento">almacenamiento</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="masa">masa</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link" href="tiempo">tiempo</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
